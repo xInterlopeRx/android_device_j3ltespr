87b6cf87ab51f9715211047bc3eda2dfe7fcebca
grace5921 <androidlover5842@gmail.com>
2016-04-11 15:59:17 +0530

    init from a5ulte

---
+++ cm.mk
@@ -0,0 +1,11 @@
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+$(call inherit-product, device/samsung/j3ltespr/full_j3ltespr.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=j3ltespr TARGET_DEVICE=a5ulte
+
+PRODUCT_NAME := cm_j3ltespr
